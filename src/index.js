d775582c18e2655dea76b20c21ccc82425ae3274
Gordon <gordon.tinyefuza@gmail.com>
2021-11-22 23:27:48 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint disable */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -8,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
