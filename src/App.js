d25004ecf94007306eacad3122f0d97e350e74da
Gordon <gordon.tinyefuza@gmail.com>
2021-11-24 19:00:19 +0300

    Fix Errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import Calculator from './components/calculator.js';
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <Calculator />
+    <Calculator />
     );
   }
 }
 
-Calculator()
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+    <App />, document.getElementById('root'),
 );
 
 React();
 App();
-// ReactDOM();
