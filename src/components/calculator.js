f617503688a0f47f6b6b6557d85aa5cded8b9d98
Gordon <gordon.tinyefuza@gmail.com>
2021-11-25 12:08:29 +0300

    Add click events to buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import '../calculator.css';
+import calculate from '../logic/calculate.js';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentValue: '0',
+      total: '0',
+      next: null,
+      operation: null,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    this.setState((previousState) => calculate(previousState, e));
   }
 
   render() {
-    const { currentValue } = this.state;
+    const { total, next, operation } = this.state;
     return (
 
 <div className="calculator">
-<div className="answer">{currentValue}</div>
+<div className="answer">{total}{operation}{next}</div>
 <div className="buttons">
-  <button type="button" className="button">AC</button>
-  <button type="button" className="button">+/-</button>
-  <button type="button" className="button">%</button>
-  <button type="button" className="button orange">&divide;</button>
-  <button type="button" className="button">7</button>
-  <button type="button" className="button">8</button>
-  <button type="button" className="button">9</button>
-  <button type="button" className="button orange">&times;</button>
-  <button type="button" className="button">4</button>
-  <button type="button" className="button">5</button>
-  <button type="button" className="button">6</button>
-  <button type="button" className="button orange">-</button>
-  <button type="button" className="button">1</button>
-  <button type="button" className="button">2</button>
-  <button type="button" className="button">3</button>
-  <button type="button" className="button orange">+</button>
-  <button type="button" className="button double-button">0</button>
-  <button type="button" className="button">.</button>
-  <button type="button" className="button orange">=</button>
+  <button type="button" id = "AC" className="button" onClick={() => this.handleClick('AC')}>AC</button>
+  <button type="button" id = "+/-" className="button" onClick={() => this.handleClick('+/-')}>+/-</button>
+  <button type="button" id = "%" className="button" onClick={() => this.handleClick('%')}>%</button>
+  <button type="button" id = "&divide;" className="button orange" onClick={() => this.handleClick('÷')}>&divide;</button>
+  <button type="button" id = "7" className="button" onClick={() => this.handleClick('7')}>7</button>
+  <button type="button" id = "8" className="button" onClick={() => this.handleClick('8')}>8</button>
+  <button type="button" id = "9" className="button" onClick={() => this.handleClick('9')}>9</button>
+  <button type="button" id = "&times;" className="button orange" onClick={() => this.handleClick('x')}>&times;</button>
+  <button type="button" id = "4" className="button" onClick={() => this.handleClick('4')}>4</button>
+  <button type="button" id = "5" className="button" onClick={() => this.handleClick('5')}>5</button>
+  <button type="button" id = "6" className="button" onClick={() => this.handleClick('6')}>6</button>
+  <button type="button" id = "-" className="button orange" onClick={() => this.handleClick('-')}>-</button>
+  <button type="button" id = "1" className="button" onClick={() => this.handleClick('1')}>1</button>
+  <button type="button" id = "2" className="button" onClick={() => this.handleClick('2')}>2</button>
+  <button type="button" id = "3" className="button" onClick={() => this.handleClick('3')}>3</button>
+  <button type="button" id = "+" className="button orange" onClick={() => this.handleClick('+')}>+</button>
+  <button type="button" id = "0" className="button double-button" onClick={() => this.handleClick('0')}>0</button>
+  <button type="button" id = "." className="button" onClick={() => this.handleClick('.')}>.</button>
+  <button type="button" id = "=" className="button orange" onClick={() => this.handleClick('=')}>=</button>
 </div>
 </div>
     );
