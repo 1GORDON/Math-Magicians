acd12b6893e730d2ed22f88b23d5e0de39563dd8
Gordon <gordon.tinyefuza@gmail.com>
2021-12-01 15:45:05 +0300

    Add Quotes.js file

---
+++ src/pages/Quotes.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import QuoteData from "../components/QuoteData";
+import '../styles/Quotes.css';
+
+function Quotes() {
+  const quotes = QuoteData();
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const RandomQuote = quotes[randomIndex];
+
+    setQuote(RandomQuote.quote);
+    setAuthor(RandomQuote.author);
+  });
+
+  return (
+    <section>
+      <p>{quote}</p>
+      <h3>{`-${author}`}</h3>
+    </section>
+  );
+}
+
+export default Quotes;
