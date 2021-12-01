778aefa28529411fb5d3e5ae1a695b6f0463e608
Gordon <gordon.tinyefuza@gmail.com>
2021-12-01 15:58:03 +0300

    Edit index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './App.css';
 
 ReactDOM.render(
-    <App />, document.getElementById('root'),
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
