3e21f8e8b58b3ac25ca4bbc809b7e1f4322da459
Gordon <gordon.tinyefuza@gmail.com>
2021-12-02 21:02:03 +0300

    Add App.test.js file

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.js';
+
+test ('renders learn react link', () => {
+  render (
+    <Router>
+      < App/>
+    </Router>,
+  );
+  const linkElement = screen.getAllByText(/Math Magicians/i);
+  expect(linkElement[0]).toBeInTheDocument();
+});
